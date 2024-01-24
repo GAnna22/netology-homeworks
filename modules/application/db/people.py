a6fa65a26b9ec6bde9b20f1a309d65c1eb99d262
GAnna_22 <gubanova.93@mail.ru>
2024-01-24 14:21:13 +0500

    deletedmodules

--- modules/application/db/people.py
+++
@@ -1,3 +0,0 @@
-def get_employees():
-    employees = ['Anna', 'Peter', 'Alexander', 'Ivan']
-    print('All employees:', ' | '.join(employees))
--- modules/application/salary.py
+++
@@ -1,3 +0,0 @@
-def calculate_salary():
-    salaries = {'Anna': 90000, 'Peter': 200000, 'Ivan': 65000, 'Alexander': 120000}
-    print('Salaries:', salaries)
